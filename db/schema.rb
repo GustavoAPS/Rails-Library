84be302618adc13c96acafd564e09c4c8bd6dd9c
Gustavo <gugustavo137@gmail.com>
2024-04-24 22:48:57 -0300

    Adding user model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_05_015938) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_25_013750) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "author"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
