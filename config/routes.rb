4d69d4338ac9752c27f18a0c2bb194581998efd6
Gustavo <gugustavo137@gmail.com>
2024-04-27 22:00:43 -0300

    Adding new user form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root "pages#home"
   resources :books#, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  resources :users, except: [:new]
+  get 'signup', to: 'users#new'
 end
